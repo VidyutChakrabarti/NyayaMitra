fa6995c5e857ca73c22e96c2d16f4b3caafbd99b
Shehzan Sheikh <sshahzan0@gmail.com>
2025-05-22 18:16:49 +0530

    Added live speech recognition and translation with audio prompts, visual feedback, and error handling to Form.jsx and Threats.jsx using Bhashini’s API for enhanced user experience.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Landing'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Landing';
+import Threats from './pages/Threats';
+import FormPage from './pages/Form';
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/threats" element={<Threats />} />
+      <Route path="/form" element={<FormPage />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
